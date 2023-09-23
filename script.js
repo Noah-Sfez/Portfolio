724450a41c7f3a8df1a0185d071279d2cc9e9afa
Noah Sfez <sfz.noah@gmail.com>
2023-09-24 00:36:33 +0200

    :construction: AJOUT PROJET

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 const slidingNewsletter = document.querySelector('.slide-in');
-let animationTriggered = false; // Variable de drapeau pour vérifier si l'animation a été déclenchée
 
 window.addEventListener('scroll', () => {
     const { scrollTop, clientHeight } = document.documentElement;
     const topElementToTopViewport = slidingNewsletter.getBoundingClientRect().top;
 
-    if (topElementToTopViewport <= clientHeight * 0.8 && !animationTriggered) {
+    if (topElementToTopViewport <= clientHeight * 0.8) {
         slidingNewsletter.classList.add('active');
-        animationTriggered = true; // Marquez l'animation comme déclenchée
+    }
+});
+
+const aeklysNewsletter = document.querySelector('.aeklys');
+
+window.addEventListener('scroll', () => {
+    const { scrollTop, clientHeight } = document.documentElement;
+    const topElementAeklysToTopViewport = aeklysNewsletter.getBoundingClientRect().top;
+
+    if (topElementAeklysToTopViewport <= clientHeight * 0.6) {
+        aeklysNewsletter.classList.add('active');
+    }
+});
+
+const mouseNewsletter = document.querySelector('.mouse');
+
+window.addEventListener('scroll', () => {
+    const { scrollTop, clientHeight } = document.documentElement;
+    const topElementMouseToTopViewport = mouseNewsletter.getBoundingClientRect().top;
+
+    if (topElementMouseToTopViewport <= clientHeight * 0.6) {
+        mouseNewsletter.classList.add('active');
     }
 });
